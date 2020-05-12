73ab44f2b4a1c1a9230db916a8af3c4da7cfb637
vineet0713 <vajoshi@Vineets-MacBook-Pro-2.local>
2020-05-12 16:45:12 -0700

    Add 2 tables for request and response feeds to the Buyer Portal.

--- BuyerPortal.java
+++ BuyerPortal.java
@@ -21,12 +21,15 @@ import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 
 public class BuyerPortal extends JPanel {
+	private String[] COLUMNS = {"Category", "Quantity", "Location"};
+	private String[] NEW_ROW = {"Click to Select Category", "Enter Quantity", "Click to Select Location"};
 	private String[] CATEGORIES = {"Masks", "Ventilators", "Pills", "Wheelchairs"};
+	private String[] LOCATIONS = {"Cupertino", "Santa Clara", "Mountain View", "San Francisco"};
 	
-	private JLabel titleLabel;
+	private JLabel titleLabel, requestsLabel, responsesLabel;
 	private JButton backButton, addEquipmentButton, submitButton;
-	private JTable dataTable;
-	private JScrollPane scrollPaneForTable;
+	private JTable addEquipmentTable, requestFeedTable, responseFeedTable;
+	private JScrollPane scrollPaneForEquipmentTable, scrollPaneForRequestTable, scrollPaneForResponseTable;
 	
 	public BuyerPortal() {
 		setLayout(null);
@@ -56,11 +59,11 @@ public class BuyerPortal extends JPanel {
 		addEquipmentButton = new JButton("+ ADD NEW EQUIPMENT");
 		addEquipmentButton.setFont(new Font("Chalkduster", Font.PLAIN, 30));
 		addEquipmentButton.setForeground(Color.DARK_GRAY);
-		addEquipmentButton.setBounds(150, 125, 425, 65);
+		addEquipmentButton.setBounds(107, 125, 425, 65);
 		addEquipmentButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				DefaultTableModel model = (DefaultTableModel)(dataTable.getModel());
-				model.addRow(new Object[]{"Click to Select Category", "Enter Quantity", "Enter Place"});
+				DefaultTableModel model = (DefaultTableModel)(addEquipmentTable.getModel());
+				model.addRow(NEW_ROW);
 			}
 		});
 		add(addEquipmentButton);
@@ -68,34 +71,87 @@ public class BuyerPortal extends JPanel {
 		submitButton = new JButton("SUBMIT ALL REQUESTS");
 		submitButton.setFont(new Font("Chalkduster", Font.PLAIN, 30));
 		submitButton.setForeground(Color.DARK_GRAY);
-		submitButton.setBounds(725, 125, 400, 65);
+		submitButton.setBounds(120, 615, 400, 65);
 		submitButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				traverseTableData();
 				// TODO: Submit the requests!
-				DefaultTableModel model = (DefaultTableModel)(dataTable.getModel());
+				DefaultTableModel model = (DefaultTableModel)(addEquipmentTable.getModel());
 				int numberOfRows = model.getRowCount();
 				for (int i = 0; i < numberOfRows; ++i) { model.removeRow(0); }
 			}
 		});
 		add(submitButton);
 		
-		dataTable = new JTable(new DefaultTableModel(new Object[]{"Category", "Quantity", "Place"}, 0));
-		dataTable.setRowHeight(50);
-		dataTable.getTableHeader().setReorderingAllowed(false);
-		dataTable.getTableHeader().setPreferredSize(new Dimension(1200, 75));
-		dataTable.getTableHeader().setFont(new Font("Serif", Font.BOLD, 25));
-		dataTable.setFont(new Font("Serif", Font.PLAIN, 20));
-		dataTable.getColumnModel().getColumn(0).setCellEditor(new DefaultCellEditor(new JComboBox<String>(CATEGORIES)));
+		addEquipmentTable = new JTable(new DefaultTableModel(COLUMNS, 0));
+		addEquipmentTable.setRowHeight(40);
+		addEquipmentTable.getTableHeader().setReorderingAllowed(false);
+		addEquipmentTable.getTableHeader().setPreferredSize(new Dimension(600, 75));
+		addEquipmentTable.getTableHeader().setFont(new Font("Serif", Font.BOLD, 30));
+		addEquipmentTable.setFont(new Font("Serif", Font.PLAIN, 15));
+		addEquipmentTable.getColumnModel().getColumn(0).setCellEditor(new DefaultCellEditor(new JComboBox<String>(CATEGORIES)));
+		addEquipmentTable.getColumnModel().getColumn(2).setCellEditor(new DefaultCellEditor(new JComboBox<String>(LOCATIONS)));
 		
-		scrollPaneForTable = new JScrollPane(dataTable);
-		scrollPaneForTable.setBounds(40, 200, 1200, 450);
-		scrollPaneForTable.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
-		add(scrollPaneForTable);
+		scrollPaneForEquipmentTable = new JScrollPane(addEquipmentTable);
+		scrollPaneForEquipmentTable.setBounds(20, 200, 600, 395);
+		scrollPaneForEquipmentTable.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+		add(scrollPaneForEquipmentTable);
+		
+		requestsLabel = new JLabel("My Requests", JLabel.CENTER);
+		requestsLabel.setFont(new Font("Chalkduster", Font.PLAIN, 30));
+		requestsLabel.setBounds(660, 125, 600, 45);
+		add(requestsLabel);
+		
+		requestFeedTable = new JTable(new DefaultTableModel(COLUMNS, 0) {
+			public boolean isCellEditable(int row, int column) { return false; }
+		});
+		requestFeedTable.setRowHeight(40);
+		requestFeedTable.getTableHeader().setReorderingAllowed(false);
+		requestFeedTable.getTableHeader().setPreferredSize(new Dimension(600, 40));
+		requestFeedTable.getTableHeader().setFont(new Font("Serif", Font.BOLD, 20));
+		requestFeedTable.setFont(new Font("Serif", Font.PLAIN, 15));
+		
+		scrollPaneForRequestTable = new JScrollPane(requestFeedTable);
+		scrollPaneForRequestTable.setBounds(660, 175, 600, 210);
+		scrollPaneForRequestTable.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+		add(scrollPaneForRequestTable);
+		
+		requestsLabel = new JLabel("Supplier Responses", JLabel.CENTER);
+		requestsLabel.setFont(new Font("Chalkduster", Font.PLAIN, 30));
+		requestsLabel.setBounds(660, 420, 600, 45);
+		add(requestsLabel);
+		
+		responseFeedTable = new JTable(new DefaultTableModel(COLUMNS, 0) {
+			public boolean isCellEditable(int row, int column) { return false; }
+		});
+		responseFeedTable.setRowHeight(40);
+		responseFeedTable.getTableHeader().setReorderingAllowed(false);
+		responseFeedTable.getTableHeader().setPreferredSize(new Dimension(600, 40));
+		responseFeedTable.getTableHeader().setFont(new Font("Serif", Font.BOLD, 20));
+		responseFeedTable.setFont(new Font("Serif", Font.PLAIN, 15));
+		
+		scrollPaneForResponseTable = new JScrollPane(responseFeedTable);
+		scrollPaneForResponseTable.setBounds(660, 470, 600, 210);
+		scrollPaneForResponseTable.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+		add(scrollPaneForResponseTable);
+		
+		addDummyData();
+	}
+	
+	// TEMPORARY METHOD TO ADD DUMMY DATA TO 2 TABLES
+	private void addDummyData() {
+		DefaultTableModel requestModel = (DefaultTableModel)(requestFeedTable.getModel());
+		for (int i = 0; i < 10; ++i) {
+			requestModel.addRow(new Object[]{"Sample Category", "Sample Quantity", "Sample Location"});
+		}
+		DefaultTableModel responseModel = (DefaultTableModel)(responseFeedTable.getModel());
+		for (int i = 0; i < 5; ++i) {
+			responseModel.addRow(new Object[]{"Sample Category", "Sample Quantity", "Sample Location"});
+		}
 	}
 	
 	private void traverseTableData() {
-		DefaultTableModel model = (DefaultTableModel)(dataTable.getModel());
+		DefaultTableModel model = (DefaultTableModel)(addEquipmentTable.getModel());
 		for (int row = 0; row < model.getRowCount(); ++row) {
 			for (int col = 0; col < model.getColumnCount(); ++col) {
 				String val = (String)(model.getValueAt(row, col));
